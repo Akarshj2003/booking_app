02476c4887e5414cfae2d63fa5ac1e1b96c8fe2a
Akarshj2003 <akarshj2003jul23@gmail.com>
2024-12-03 18:29:30 +0530

    Back end user funcion complet update login etc

--- Back/routes/user-routes.js
+++ Back/routes/user-routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllUsers, signup } from "../controllers/user-controllers.js";
+import { deleteUser, getAllUsers, login, signup, updateUser } from "../controllers/user-controllers.js";
 
 
 
@@ -7,5 +7,8 @@ const userRouter =express.Router();
 
 userRouter.get("/",getAllUsers);
 userRouter.post("/signup",signup);
+userRouter.put("/:id",updateUser);
+userRouter.delete("/:id",deleteUser);
+userRouter.post("/login",login);
 
 export default userRouter;
